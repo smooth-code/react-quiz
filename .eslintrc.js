d56004ab788940dad299656702288f09d2ccfdf1
Greg Bergé <berge.greg@gmail.com>
2018-01-04 10:55:26 +0000

    chore(setup): add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 7,
+    sourceType: 'module',
+  },
+  env: {
+    jest: true,
+    browser: true,
+  },
+  rules: {
+    'no-shadow': 'off',
+    'no-nested-ternary': 'off',
+
+    'react/jsx-filename-extension': ['error', { extensions: ['.js'] }],
+    'react/prop-types': 'off',
+    'react/require-default-props': 'off',
+
+    'import/prefer-default-export': 'off',
+    'import/no-unresolved': 'warning',
+    'import/extensions': 'warning',
+
+    'class-methods-use-this': 'off',
+
+    'jsx-a11y/anchor-is-valid': 'off',
+    'jsx-a11y/mouse-events-have-key-events': 'warn',
+    'jsx-a11y/click-events-have-key-events': 'warn',
+    'prefer-promise-reject-errors': 'warn',
+  },
+}
